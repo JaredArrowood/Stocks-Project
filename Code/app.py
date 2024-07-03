0220c8dbb7402fbd718c7ffcf057ac6071eb3f73
JaredArrowood <jarrow1@lsu.edu>
2024-07-03 13:21:43 -0500

    added yfinance in requirements

--- Code/app.py
+++ Code/app.py
@@ -1,6 +1,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
+
+
+# ticker = input("What is the name or ticker of the stock you would like to analyze?: ")
+
+# print(ticker)
+
+# stock = yf.Ticker(ticker)
+
+# info = stock.info
+
+# if info:
+#     print("Stock information found!")
+# else:
+#     print("Stock information not found, please check if ticker is correct.")
+
+# balance_sheet = stock.balance_sheet
+
+# print(balance_sheet)
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Stocks-Project
 
 ## Prerequisites
+
 python3 -m venv venv
 .\venv\Scripts\activate.bat
 
 pip install -r requirements.txt
+
+## Goals
+
+1. Have user enter a stock ticker or name
+2. Run that stock through a Machine Learning program that will interpret the balance sheet of that stock and determine if it is a good buy or not
+3. Create a website that will have a place for the user to enter the stock name or ticker and will display the results
