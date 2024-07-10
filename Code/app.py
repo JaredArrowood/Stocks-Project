576134913716e6c15e365d8cf808f5172a1845a1
JaredArrowood <jarrow1@lsu.edu>
2024-07-10 16:03:35 -0500

    started notebook for ml model

--- Code/app.py
+++ Code/app.py
@@ -2,24 +2,50 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+from yfinance.exceptions import YFException
+from requests.exceptions import HTTPError
 
 
-# ticker = input("What is the name or ticker of the stock you would like to analyze?: ")
 
-# print(ticker)
 
-# stock = yf.Ticker(ticker)
 
-# info = stock.info
 
-# if info:
-#     print("Stock information found!")
-# else:
-#     print("Stock information not found, please check if ticker is correct.")
+found = False
 
-# balance_sheet = stock.balance_sheet
+#loop that will continually ask the user to input their ticker (update to take names as well as tickers)
+while(not found):
 
-# print(balance_sheet)
+    ticker = input("What is the ticker of the stock you would like to analyze?: ")
+    print(ticker)
+
+    stock = yf.Ticker(ticker)
+
+    try:
+
+        balance_sheet = stock.balance_sheet
+
+        if(balance_sheet.empty):
+            raise ValueError("Stock info not found, please recheck your ticker symbol")
+        else:
+            found = True
+            print(balance_sheet)
+
+    except ValueError as ve:
+        print("Stock information not found, please check if the ticker was entered correctly.")
+    
+    except HTTPError as http_err:
+        if http_err.response.status_code == 404:
+            print("HTTP Error: 404 Not Found. The ticker symbol may be incorrect")
+        else:
+            print(f"HTTP Error occurred: {http_err}")
+
+
+
+    
+
+
+
+
 
 
 
